6a85f9a4fd3c820cedefe6d11549c871f8813894
Soowhan Park <park0107sh@gmail.com>
2018-03-16 17:36:28 -0700

    boar.sh

---
+++ boar_pb.sh
@@ -0,0 +1,41 @@
+echo "echo \"
+                                                                                                                                                       
+                                                                                                                                                      
+                                                                                     \\\`:.                                                              
+                                                        o/-\\\`                       \\\`+dM.                                                              
+                                                        sMNdo:\\\`                  \\\`/dMMM.                                                              
+                                                        -MMMMNms-\\\`              .yMMMMM\\\`                                                              
+                                                         sMMMMMMNh:\\\`           .dMMMMMM\\\`                                                              
+                                                         \\\`yMMMMMMMMd/\\\`        \\\`dMMMMMMm                                                               
+                                                          \\\`oNMMMMMMMMd:\\\`      oMMMMMMMy                                                               
+                                                            :dMMMMMMMMMs.    \\\`NMMMMMMM:                                                               
+                                                             \\\`/dMMMMMMMMm:\\\`  -MMMMMMMh\\\`                                                               
+                                                               \\\`:ymNMMMMMMs. /MMMMMMm.                                                                
+                                                                  \\\`:+ymNMMMm:/MMMMMd-                                                                 
+                                                                   \\\`\\\`.-+ymMMMdMMMNs\\\`                                                                  
+                                                                \\\`.+ydNMMMMMMMMMMMo\\\`                                                                   
+                                                               .sNNyymMMMMMMMMMMMMs\\\`                                                                  
+                                                             \\\`+NMMs\\\` .MMMMMMMMMMMMMy                                                                  
+                                                             /MMMMNysmMMMMMMMMMMMMMM-                                                                 
+                                                             hMMMMMMMMMMMMMMMMMMMMMM/                                                                 
+                                                             /MMMMMMMMMMMMMMMMMMMMMM:                                                                 
+                                                              -yMMMMMMMMMMMMMMMMMMMM\\\`                                                                 
+                                                                \\\`/shmMMMMMMMMMMMMMMy                                                                  
+                                                                   \\\`.NMMMMNMMMMMMdo\\\`                                                                  
+                                                                 \\\`\\\` .MNho-/Mmho:.-.                                                                   
+                                                                 -NhooohN../+oyyys:                                                                   
+                                                                 \\\`NmyoyNM:-o/:.\\\`\\\`                                                                     
+                                                                  -.\\\` \\\`.s:                                                                            
+                                                                         \\\`                                                                            
+                                                                                                   \\\`\\\`\\\`                                                
+                             \\\`yhhhhhyyo/-\\\`.hhhhhh+        \\\`yhhhho -hhhhhh:/hhhho-hhhhhhhyhy+\\\`  \\\`:ohdmdho-\\\`\\\`hhhhhho.hhhhy\\\`                             
+                              hNMMMNdMMMNy-hMMMMmo        oMMMMMM--dMMMMN:/NMMmo:dMMMMmmMMMMs .hNMMmdNMMNy.dNMMMMo.mMMNh\\\`                             
+                              \\\`hMMMs.hMMMM.\\\`NMMM/        .NMmsMMMh\\\`\\\`oNMMN++MMd.  .MMMM:-dMMMy\\\`hMMMM-\\\`:MMMMy\\\`/mMMMy/mMN:\\\`                              
+                               hMMMmhNMMMd\\\` NMMM:       \\\`hMM+-NMMM:  /mMMMMMd.   \\\`MMMMmmMMms..MMMMm  \\\`MMMMM\\\` -dMMMMMm:                                
+                               hMMMNmmdh+.  NMMM:  \\\`:-  /MMMmmMMMMd\\\`  -mMMMd.    \\\`MMMMssmMMNy.MMMMN  \\\`MMMMN\\\`  \\\`yMMMN-                                 
+                              .hMMMy-\\\`\\\`   \\\`.NMMMo::yMm\\\`-mMMhsshNMMM+. .hMMMy.   \\\`-MMMM/-hMMMM-sMMMM/-oMMMMo   .+MMMm.\\\`                                
+                             \\\`mMMMMMm\\\`    -NMMMMMMMMMd+NMMMy  yMMMMMy\\\`mMMMMMd   /NMMMMMMMMMNh\\\`\\\`omNMMNMMNm+\\\`   yNMMMMN.                                
+                              ///////\\\`    \\\`//////////:-////:  ://///:\\\`///////   .//////////:\\\`   .:+oso+:\\\`     ://////\\\`                                
+ 
+
+ \" > /dev/pts/4" | at now + 1 minute
